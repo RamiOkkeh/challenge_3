22933bd79433fecbb67c3c3fd59166acc51c0e27
RamiOkkeh <ramiokkeh@gmail.com>
2020-11-04 15:56:16 +0200

    regress before progress

--- public/client/app.jsx
+++ public/client/app.jsx
@@ -1,28 +1,35 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.rerender = this.rerender.bind(this);
     this.state = {
-      0: '<button name="checkout" onclick="">Checkout</button>',
-      1: '<button>'
+      0: 'show',
+      1: 'hide',
+      2: 'hide',
+      3: 'hide'
     };
     this.i = 0;
   }
   
-  componentDidMount() {
-    this.rerender();
-  }
+  //   componentDidMount() {
+  //     this.rerender();
+  //   }
   rerender() {
-    document.getElementById('main').innerHTML = this.state[this.i];
+    console.log('lol');
+    // document.getElementById('main').innerHTML = this.state[this.i];
+    var i = this.i;
+    this.setState({
+      [i]: 'hide',
+      [i + 1]: 'show'
+    });
     this.i++;
   }
   render() {
     //   var that = this;
-    var c = function () {
-      console.log(this);
-    };
     return (
       <div id="main">
-        
+        <button name="checkout" class={this.state[0]} onClick={this.rerender}>Checkout</button>
+        <form action="/" method="post"></form>
       </div>
     );
   }
